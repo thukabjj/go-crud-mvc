29cde3c7244db8739debe80bdbbb395d30d327ee
Arthur Alves <arthur.alvesdeveloper@outlook.com>
2021-09-12 21:59:37 -0300

    fix connection with database setting host

--- db/db.go
+++ db/db.go
@@ -2,13 +2,15 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"os"
 
 	_ "github.com/lib/pq"
 )
 
 func ConnectComBanco() *sql.DB {
-	conexao := "user=" + os.Getenv("POSTGRES_USER") + " password=" + os.Getenv("POSTGRES_PASSWORD") + " dbname=" + os.Getenv("POSTGRES_DB") + " sslmode=" + os.Getenv("POSTGRES_SSL_MODE")
+	conexao := "user=" + os.Getenv("POSTGRES_USER") + " password=" + os.Getenv("POSTGRES_PASSWORD") + " host=" + os.Getenv("POSTGRES_HOST") + " dbname=" + os.Getenv("POSTGRES_DB") + " sslmode=" + os.Getenv("POSTGRES_SSL_MODE")
+	fmt.Println("Connect: ", conexao)
 	db, err := sql.Open(os.Getenv("POSTGRES_DRIVER_NAME"), conexao)
 	if err != nil {
 		panic(err.Error())
